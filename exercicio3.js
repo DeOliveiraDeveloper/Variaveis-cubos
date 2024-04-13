5a902b67cf0b2b594cadc2a3b0cf02c155b0bb75
DeOliveiraDeveloper <monteirocaio4@gmail.com>
2024-04-13 19:39:19 -0300

    Exercício 3

---
+++ exercicio3.js
@@ -0,0 +1,8 @@
+// 1. Declare uma variável nome e uma idade;
+// 2. Imprima a frase: "Meu nome é XXX e tenho XXX anos!"
+
+let grad = "Cb"
+let nomeGuerra = "De Oliveira"
+let idade = 20
+
+console.log(`Meu nome é ${nomeGuerra}, sou ${grad} do Exército Brasileiro e tenho ${idade} anos!`)
