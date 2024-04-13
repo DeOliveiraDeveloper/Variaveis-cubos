b0cbaf6de9abf39552588e47844bd19602efda58
DeOliveiraDeveloper <monteirocaio4@gmail.com>
2024-04-13 19:45:27 -0300

    Exercício 4

---
+++ exercicio4.js
@@ -0,0 +1,10 @@
+// 1. Dado o código abaixo, refatore usando template string;
+// 2. Imprima a frase: "Meu nome é XXX e tenho XXX anos!"
+
+let grad = "Cb"
+let nomeGuerra = "De Oliveira"
+let idade = 20
+
+let frase = `Meu nome é ${nomeGuerra}, sou ${grad} do Exército Brasileiro e tenho ${idade} anos!`
+
+console.log(frase)
